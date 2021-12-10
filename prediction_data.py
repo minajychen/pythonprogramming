8c48276cee9daa813b2a98d60a84978b0c7abaef
minajychen <86486074+minajychen@users.noreply.github.com>
2021-12-10 12:55:28 -0600

    Add files via upload

--- prediction_data.py
+++ prediction_data.py
@@ -9,14 +9,36 @@ class PredictionData():
     def __init__(self,file):
         self.medinc=(np.loadtxt(file,delimiter=',',skiprows=1,dtype=float,usecols=(0))).tolist()
         self.houseprice=(np.loadtxt(file,delimiter=',',skiprows=1,dtype=float,usecols=(8))).tolist()
-        plt.scatter(self.houseprice,self.medinc)
-        plt.title("California house prices vs. medium income")
-        plt.xlabel('Housing prices')
-        plt.ylabel('Medium income')
+        plt.scatter(self.medinc,self.houseprice)
+        plt.title("California medium income vs. housing prices")
+        plt.xlabel('Medium income')
+        plt.ylabel('Housing prices')
         plt.show()
 
     def standardize_data(self,data):
-        #how do I standardize the target?
-        return stats.zscore(data)
+        """function that standardizes the data"""
+        data=np.array(data)
+        return (data-data.mean())/(data.std())
+    
+    def linear_reg_model1(self):
+        """Perform linear regression to compute β in order to attain the linear model: y¯ = βn"""
+        stnd_y=np.array(self.standardize_data(self.houseprice))
+        stnd_x=np.array(self.standardize_data(self.medinc))
+        x_transformed=stnd_x[:,np.newaxis]
+        beta, _, _, _ = np.linalg.lstsq(x_transformed, stnd_y,rcond=None)
+        plt.plot(x_transformed,stnd_y,'bo')
+        plt.plot(x_transformed,beta*x_transformed,'r-')
+        plt.show()
+        return
 
-p=PredictionData("california house prices.csv")
+    def linear_reg_model2(self):
+        """linear regression to fit the moving avg to a linear model y¯ = β0 + β1n."""
+        stnd_x=np.array(self.standardize_data())
+        x_transformed=np.vstack([stnd_x,np.ones(len(stnd_x))]).T
+        stnd_y=np.array(self.standardize_data(self.houseprice))
+        beta_1, beta_0 = np.linalg.lstsq(x_transformed,stnd_y,rcond=None)[0]
+        plt.plot(x_transformed,stnd_y,'bo')
+        plt.plot(x_transformed,beta_1*x_transformed+beta_0,'r-')
+        plt.show()
+        return 
+
